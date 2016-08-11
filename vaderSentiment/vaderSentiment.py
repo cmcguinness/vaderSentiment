b2608dee70ce4efab4a8408281212bc015f27133
Charles McGuinness <github@mcguinness.us>
2016-08-11 09:51:36 -0400

    Incorporate use of set per kennyjoseph

--- vaderSentiment/vaderSentiment.py
+++ vaderSentiment/vaderSentiment.py
@@ -45,14 +45,14 @@ REGEX_REMOVE_PUNCTUATION = re.compile('[%s]' % re.escape(string.punctuation))
 
 PUNC_LIST = [".", "!", "?", ",", ";", ":", "-", "'", "\"",
                 "!!", "!!!", "??", "???", "?!?", "!?!", "?!?!", "!?!?"]
-NEGATE = ["aint", "arent", "cannot", "cant", "couldnt", "darent", "didnt", "doesnt",
+NEGATE = set(["aint", "arent", "cannot", "cant", "couldnt", "darent", "didnt", "doesnt",
               "ain't", "aren't", "can't", "couldn't", "daren't", "didn't", "doesn't",
               "dont", "hadnt", "hasnt", "havent", "isnt", "mightnt", "mustnt", "neither",
               "don't", "hadn't", "hasn't", "haven't", "isn't", "mightn't", "mustn't",
               "neednt", "needn't", "never", "none", "nope", "nor", "not", "nothing", "nowhere",
               "oughtnt", "shant", "shouldnt", "uhuh", "wasnt", "werent",
               "oughtn't", "shan't", "shouldn't", "uh-uh", "wasn't", "weren't",
-              "without", "wont", "wouldnt", "won't", "wouldn't", "rarely", "seldom", "despite"]
+              "without", "wont", "wouldnt", "won't", "wouldn't", "rarely", "seldom", "despite"])
 # booster/dampener 'intensifiers' or 'degree adverbs' http://en.wiktionary.org/wiki/Category:English_degree_adverbs
 BOOSTER_DICT = {"absolutely": B_INCR, "amazingly": B_INCR, "awfully": B_INCR, "completely": B_INCR, "considerably": B_INCR,
                 "decidedly": B_INCR, "deeply": B_INCR, "effing": B_INCR, "enormously": B_INCR,
